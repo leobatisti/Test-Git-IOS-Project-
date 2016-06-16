a3d111bc81f591f284e6edc3898581387cd07f89
Leonardo Batisti de Faria <leobatisti@Roots-Mac.local>
2016-06-16 09:21:11 -0300

    added a test class

---
+++ CCtest.h
@@ -0,0 +1,13 @@
+//
+//  CCtest.h
+//  Git Test
+//
+//  Created by Leonardo Batisti de Faria on 16/06/16.
+//  Copyright © 2016 Leonardo Batisti de Faria. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCtest : NSObject
+
+@end
---
+++ CCtest.m
@@ -0,0 +1,13 @@
+//
+//  CCtest.m
+//  Git Test
+//
+//  Created by Leonardo Batisti de Faria on 16/06/16.
+//  Copyright © 2016 Leonardo Batisti de Faria. All rights reserved.
+//
+
+#import "CCtest.h"
+
+@implementation CCtest
+
+@end
